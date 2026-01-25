1c29d29067a56042a190cb421413551af25b7360
RanveerArnavkamboj <ranbeerkamboj45@gmail.com>
2026-01-25 14:17:06 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Minimal service worker
-self.addEventListener('install', function(e) {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', function(e) {
-  // You can add caching logic later
-});
